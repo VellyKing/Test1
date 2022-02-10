2942c813f382504f02d9599f42c459fea402423b
KingSchoolAccount <marcellones.vellkingjasper@dnsc.edu.ph>
2022-02-10 11:44:37 +0800

    Test

---
+++ src/test1/Test1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Velly King Jasper O. Marcellones
+ */
+public class Test1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("First Test");
+    }
+    
+}
